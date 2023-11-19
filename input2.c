74ccb8c89584a5a116a3a8e7cdd162c3020b8c10
artem <barishartem@gmail.com>
2023-11-19 17:49:54 +0200

    add calculator

--- input2.c
+++ input2.c
@@ -1,3 +1,18 @@
-//
-// Created by Artem on 11/18/2023.
-//
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int v=1;
+    int a=3;
+    int v;
+    if(v > a) {
+        if(v > a) {
+            printf("noturaaaaa");
+            a = v - 1;
+        }
+    }
+    else {
+        printf("uraaaaaa");
+    }
+}
+
